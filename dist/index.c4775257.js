ec8d8b62628664ad54991d86f813d36639e72d0e
Ana Cervantes <anac2828@yahoo.com>
2023-04-18 08:38:05 -0700

    first deploymnent

---
+++ dist/index.c4775257.js
@@ -0,0 +1,11 @@
+const accordion = document.querySelector(".accordion");
+accordion.addEventListener("click", (e)=>{
+    const activePanel = accordion.querySelector('[aria-expanded="true"]');
+    if (!e.target.closest(".accordion__panel")) return;
+    const selectedPanel = e.target.closest(".accordion__panel").querySelector(".accordion__trigger");
+    let isOpen = selectedPanel.getAttribute("aria-expanded") === "true" ? "false" : "true";
+    selectedPanel.setAttribute("aria-expanded", isOpen);
+    if (activePanel) activePanel.setAttribute("aria-expanded", "false");
+});
+
+//# sourceMappingURL=index.c4775257.js.map
